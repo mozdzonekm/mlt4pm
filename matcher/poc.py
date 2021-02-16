f1fcee44efddd20b8cf79d8880943d4a02a4dea0
Michal Mozdzonek <michaljmozdzonek@gmail.com>
2021-02-16 14:37:25 +0100

    Matrices for series

--- matcher/poc.py
+++ matcher/poc.py
@@ -11,26 +11,29 @@ DATA_DIR = '../data'
 
 dataset_path = os.path.join(DATA_DIR, 'computers_train', 'computers_train_small.json.gz')
 df = pd.read_json(dataset_path, compression='gzip', lines=True)
-IDS = 1350
-label = df.loc[IDS, 'label']
-title_left = df.loc[IDS, 'title_left']
-title_right = df.loc[IDS, 'title_right']
+label = df.loc[:3, 'label'].tolist()
+title_left = df.loc[:3, 'title_left'].tolist()
+title_right = df.loc[:3, 'title_right'].tolist()
 
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 model = TFRobertaModel.from_pretrained('roberta-base')
 
-inputs = tokenizer([title_left, title_right], return_tensors='tf', padding=True)
+inputs_left = tokenizer(title_left, return_tensors='tf', padding=True)
+outputs_left = model(inputs_left)
+rep_left = outputs_left[0]
 
-outputs = model(inputs)
-representation = outputs[0]
+inputs_right = tokenizer(title_right, return_tensors='tf', padding=True)
+outputs_right = model(inputs_right)
+rep_right = outputs_right[0]
 
-similarity_matrix = tf.tensordot(representation[0], tf.transpose(representation[1]), axes=1)
+similarity_matrices = [tf.tensordot(L, tf.transpose(R), axes=1) for L, R in zip(rep_left, rep_right)]
 
-plt.matshow(similarity_matrix)
+print(tf.shape(similarity_matrices[0]))
+plt.matshow(similarity_matrices[0])
 plt.show()
 
-print(title_left)
-print(title_right)
-print('similar' if label == 1 else 'different')
+print(title_left[0])
+print(title_right[0])
+print('similar' if label[0] == 1 else 'different')
 
 
